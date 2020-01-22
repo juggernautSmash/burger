cd24b588f3037c8dc602d12c4569817329f15810
Julius Casipit <52521862+juggernautSmash@users.noreply.github.com>
2020-01-21 20:47:14 -0800

    configured for deploying in heroku

--- config/index.js
+++ config/index.js
@@ -1,6 +1,2 @@
-module.exports = require('mysql2').createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'burger_db'
-})
+const Sequelize = require('sequelize')
+module.exports = new Sequelize(process.env.NODE_ENV ? process.env.JAWSDB_URL : process.env.LOCALDB_URL)
