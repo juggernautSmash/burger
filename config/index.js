fa19e04d61a8122d889e1f51802da100cbdc1a41
Julius Casipit <52521862+juggernautSmash@users.noreply.github.com>
2020-01-21 21:13:39 -0800

    removed sequelize

--- config/index.js
+++ config/index.js
@@ -1,2 +1,6 @@
-const Sequelize = require('sequelize')
-module.exports = new Sequelize(process.env.NODE_ENV ? process.env.JAWSDB_URL : process.env.LOCALDB_URL)
+module.exports = require('mysql2').createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'burger_db'
+})
